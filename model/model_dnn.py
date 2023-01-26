c0f372858a920ed7fcd527271afc30318cc6beb7
dhingratul <dhingra.atul92@gmail.com>
2023-01-25 22:25:20 -0800

    dnn mvp

---
+++ model/model_dnn.py
@@ -0,0 +1,249 @@
+from __future__ import unicode_literals, print_function, division
+import pandas as pd
+import os
+from io import open
+import glob
+import os
+import unicodedata
+import string
+import torch
+import torch.nn as nn
+import random
+import time
+import math
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+n_iters = 100000
+print_every = 5000
+plot_every = 1000
+all_letters = string.ascii_letters + " .,;'"
+n_letters = len(all_letters)
+MIN_OCCURANCE = 3
+criterion = nn.NLLLoss()
+learning_rate = (
+    0.005  # If you set this too high, it might explode. If too low, it might not learn
+)
+base_dir = "/Users/dhingratul/Documents/instate_data"
+fid = "instate_selected.csv.gz"
+
+
+def preprare_data(df):
+    category_lines = (
+        df.groupby(["state"])["last_name"]
+        .apply(lambda grp: list(grp.value_counts().index))
+        .to_dict()
+    )
+    all_categories = list(category_lines.keys())
+    return category_lines, all_categories
+
+
+def process_data(base_path):
+    df = pd.read_csv(base_path)
+    df = df[df.last_name != "LNU"]  # Remove last name unknows
+    df = df[
+        df.groupby("last_name")["last_name"].transform("count").ge(MIN_OCCURANCE)
+    ]  # Remove all last names that occur less than 3 times
+    return df
+
+
+# Data
+print("Processing data")
+df = process_data(os.path.join(base_dir, fid))
+# Model Init
+category_lines, all_categories = preprare_data(df)
+
+# Turning Names into Tensors
+# Find letter index from all_letters, e.g. "a" = 0
+def letterToIndex(letter):
+    return all_letters.find(letter)
+
+
+# Just for demonstration, turn a letter into a <1 x n_letters> Tensor
+def letterToTensor(letter):
+    tensor = torch.zeros(1, n_letters)
+    tensor[0][letterToIndex(letter)] = 1
+    return tensor
+
+
+# Turn a name into a <name_length x 1 x n_letters>,
+# or an array of one-hot letter vectors
+def nameToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][letterToIndex(letter)] = 1
+    return tensor
+
+
+# Network Definition
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+
+def model_init(dic):
+    n_categories = len(dic)
+    n_hidden = 128
+    rnn = RNN(n_letters, n_hidden, n_categories)
+    return rnn
+
+
+rnn = model_init(category_lines)
+n_categories = len(category_lines)
+
+# Util
+def categoryFromOutput(output):
+    top_n, top_i = output.topk(1)
+    category_i = top_i[0].item()
+    return all_categories[category_i], category_i
+
+
+# Training Data
+def randomChoice(l):
+    return l[random.randint(0, len(l) - 1)]
+
+
+def randomTrainingExample():
+    category = randomChoice(all_categories)
+    line = randomChoice(category_lines[category])
+    category_tensor = torch.tensor([all_categories.index(category)], dtype=torch.long)
+    line_tensor = nameToTensor(line)
+    return category, line, category_tensor, line_tensor
+
+
+def train(category_tensor, line_tensor):
+    hidden = rnn.initHidden()
+
+    rnn.zero_grad()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    loss = criterion(output, category_tensor)
+    loss.backward()
+
+    # Add parameters' gradients to their values, multiplied by learning rate
+    for p in rnn.parameters():
+        p.data.add_(p.grad.data, alpha=-learning_rate)
+
+    return output, loss.item()
+
+
+# Training
+# Keep track of losses for plotting
+current_loss = 0
+all_losses = []
+
+
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return "%dm %ds" % (m, s)
+
+
+start = time.time()
+
+for iter in range(1, n_iters + 1):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    output, loss = train(category_tensor, line_tensor)
+    current_loss += loss
+
+    # Print iter number, loss, name and guess
+    if iter % print_every == 0:
+        guess, guess_i = categoryFromOutput(output)
+        correct = "✓" if guess == category else "✗ (%s)" % category
+        print(
+            "%d %d%% (%s) %.4f %s / %s %s"
+            % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct)
+        )
+
+    # Add current loss avg to list of losses
+    if iter % plot_every == 0:
+        all_losses.append(current_loss / plot_every)
+        current_loss = 0
+
+# plot Losses
+plt.figure()
+plt.plot(all_losses)
+
+
+# Eval
+# Keep track of correct guesses in a confusion matrix
+confusion = torch.zeros(n_categories, n_categories)
+n_confusion = 10000
+
+# Just return an output given a line
+def evaluate(line_tensor):
+    hidden = rnn.initHidden()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    return output
+
+
+# Go through a bunch of examples and record which are correctly guessed
+for i in range(n_confusion):
+    category, line, category_tensor, line_tensor = randomTrainingExample()
+    output = evaluate(line_tensor)
+    guess, guess_i = categoryFromOutput(output)
+    category_i = all_categories.index(category)
+    confusion[category_i][guess_i] += 1
+
+# Normalize by dividing every row by its sum
+for i in range(n_categories):
+    confusion[i] = confusion[i] / confusion[i].sum()
+
+# Set up plot
+fig = plt.figure()
+ax = fig.add_subplot(111)
+cax = ax.matshow(confusion.numpy())
+fig.colorbar(cax)
+
+# Set up axes
+ax.set_xticklabels([""] + all_categories, rotation=90)
+ax.set_yticklabels([""] + all_categories)
+
+# Force label at every tick
+ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+# sphinx_gallery_thumbnail_number = 2
+plt.show()
+
+# Predict
+def predict(input_line, n_predictions=3):
+    print("\n> %s" % input_line)
+    with torch.no_grad():
+        output = evaluate(nameToTensor(input_line))
+
+        # Get top N categories
+        topv, topi = output.topk(n_predictions, 1, True)
+        predictions = []
+
+        for i in range(n_predictions):
+            value = topv[0][i].item()
+            category_index = topi[0][i].item()
+            print("(%.2f) %s" % (value, all_categories[category_index]))
+            predictions.append([value, all_categories[category_index]])
+
+
+predict("DATTA")
