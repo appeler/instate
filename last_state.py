140708ffd5ffb655c764b0e97ca78c6fdae4b531
***** <gsood07@gmail.com>
2023-03-12 12:58:42 -0700

    move folders

--- last_state.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def last_state(df, namecol):
-	download_file()
-	state_name = pd.read_csv("data/state_to_languages.csv")
-    res = df.merge(state_name, left_on=namecol, right_on='state', how='left')
-    return(res)
