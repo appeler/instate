4fc27225382c8abd5729fec43a099a877592fe49
***** <721466+soodoku@users.noreply.github.com>
2025-11-22 21:32:47 -0800

    fix conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,14 @@ extensions = [
     'sphinx.ext.napoleon',
 ]
 
+# Autodoc configuration
+autodoc_mock_imports = ['Levenshtein', 'tensorflow', 'torch', 'torchvision', 'torchdata', 'torchtext']
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -57,7 +65,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
